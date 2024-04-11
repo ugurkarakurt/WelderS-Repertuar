fe75f04a783171602edd0bcde67d3835dbe993fc
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-04-11 19:15:09 +0300

    .

--- components/section/section.component.tsx
+++ components/section/section.component.tsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./section.module.scss";
 import { Song } from "@/types";
+import Image from 'next/image';
+import DropdownIcon from "@/public/caret-down.png";
 
 interface Props {
   song: Song;
 }
 
 const Section: React.FC<Props> = ({ song }) => {
+  const [isChordsVisible, setIsChordsVisible] = useState(true);
+
+  const toggleChordsVisibility = () => {
+    setIsChordsVisible(prevState => !prevState);
+  };
 
   return (
     <div className={styles.sectionContainer}>
-      <h2>{song.id}. {song.artist} - {song.title}</h2>
-      {song.chordsHTML && (
-        <>
-          {
-            song.chordsHTML
-          }
-        </>
+      <h2 onClick={toggleChordsVisibility}>
+        {song.id}. {song.artist} - {song.title}
+        <Image className={`${isChordsVisible && styles.active}`} width={20} height={20} src={DropdownIcon} alt="icon" />
+      </h2>
+      {isChordsVisible && song.chordsHTML && (
+        <div>
+          {song.chordsHTML}
+        </div>
       )}
     </div>
   );
