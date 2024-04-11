8a85a1f5fce2f10649c11c17ce3dfd7f7acaad48
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-04-11 20:07:27 +0300

    ç

--- components/section/section.component.tsx
+++ components/section/section.component.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+// Section.tsx
+"use client"
+import React, { useState, useEffect, useRef } from 'react';
 import styles from "./section.module.scss";
 import { Song } from "@/types";
 import Image from 'next/image';
@@ -10,13 +12,22 @@ interface Props {
 
 const Section: React.FC<Props> = ({ song }) => {
   const [isChordsVisible, setIsChordsVisible] = useState(true);
+  const sectionRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (sectionRef.current) {
+      const sectionHeight = sectionRef.current.offsetHeight;
+      // HomeContainer bileşenine bölümün yüksekliğini aktar
+      // sectionHeight değişkeni, HomeContainer bileşeninde kullanılacak
+    }
+  }, []);
 
   const toggleChordsVisibility = () => {
     setIsChordsVisible(prevState => !prevState);
   };
 
   return (
-    <div className={styles.sectionContainer}>
+    <div className={styles.sectionContainer} ref={sectionRef}>
       <h2 onClick={toggleChordsVisibility}>
         {song.id}. {song.artist} - {song.title}
         <Image className={`${isChordsVisible && styles.active}`} width={20} height={20} src={DropdownIcon} alt="icon" />
--- containers/home.container.tsx
+++ containers/home.container.tsx
@@ -1,17 +1,20 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from "./home.module.scss";
-import { Song } from "@/types";
 import Section from '@/components/section/section.component';
 import { songs } from "@/data/songs";
+import Image from 'next/image';
+import PlayIcon from "@/public/play.png";
+import PauseIcon from "@/public/pause.png";
+import AddIcon from "@/public/add.png";
+import RemoveIcon from "@/public/remove.png";
 
 const HomeContainer = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [isAutoScrolling, setIsAutoScrolling] = useState<boolean>(false);
+  const [scrollSpeed, setScrollSpeed] = useState<number>(200);
 
-  const handleSearch = (event: any) => {
-    setSearchTerm(event.target.value);
-  };
-
+  const scrollInterval = useRef<NodeJS.Timeout | null>(null);
   const filteredSongs = songs.filter(song => {
     const normalizedSearchTerm = searchTerm.toLowerCase();
     return (
@@ -19,6 +22,37 @@ const HomeContainer = () => {
       song.artist.toLowerCase().includes(normalizedSearchTerm)
     );
   });
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const toggleAutoScroll = () => {
+    setIsAutoScrolling(!isAutoScrolling);
+  };
+
+  const increaseScrollSpeed = () => {
+    setScrollSpeed(prevSpeed => prevSpeed - 20);
+  };
+
+  const decreaseScrollSpeed = () => {
+    setScrollSpeed(prevSpeed => prevSpeed + 20);
+  };
+
+  useEffect(() => {
+    if (isAutoScrolling) {
+      scrollInterval.current = setInterval(() => {
+        window.scrollBy(0, 1);
+      }, scrollSpeed);
+    } else if (scrollInterval.current) {
+      clearInterval(scrollInterval.current);
+    }
+
+    // Cleanup fonksiyonu
+    return () => {
+      if (scrollInterval.current) clearInterval(scrollInterval.current);
+    };
+  }, [isAutoScrolling, scrollSpeed]);
 
 
   return (
@@ -33,10 +67,15 @@ const HomeContainer = () => {
       {filteredSongs.map(song => (
         <Section key={song.id} song={song} />
       ))}
+      <div className={styles.scrollButton}>
+        <button onClick={increaseScrollSpeed}><Image src={AddIcon} width={30} height={30} alt="add_icon" /></button>
+        <button onClick={toggleAutoScroll}>
+          {!isAutoScrolling ? (<Image src={PlayIcon} width={30} height={30} alt="play_icon" />
+          ) : (<Image src={PauseIcon} width={30} height={30} alt="pause_icon" />)}
 
-      {/* {songsList.map((song: Song) => (
-        <Section key={song.id} song={song} />
-      ))} */}
+        </button>
+        <button onClick={decreaseScrollSpeed}><Image src={RemoveIcon} width={30} height={30} alt="remove_icon" /></button>
+      </div>
     </div>
   );
 };
