12b3f24b2ef6b864082fd74b81635ce20a6eaf49
ugurkarakurt <ugurkarakurt57@gmail.com>
2024-11-07 21:43:51 +0300

    asdasd

--- components/section/section.component.tsx
+++ components/section/section.component.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 import DropdownIcon from "@/public/caret-down.png";
 
 interface Props {
-  song: Song;
+  song: any;
 }
 
 const Section: React.FC<Props> = ({ song }) => {
